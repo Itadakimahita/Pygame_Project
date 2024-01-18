c19eb3dcec7be74d7ed564e34fbb8a89e8999d37
Dias Kadyrov <kadirov.dias@mail.ru>
2024-01-19 02:46:59 +0600

    left and right attack

--- weapon.py
+++ weapon.py
@@ -3,8 +3,17 @@ import pygame
 class Weapon(pygame.sprite.Sprite):
     def __init__(self, player, groups):
         super().__init__(groups)
+        direction = player.status.split('_')[0]
 
-        direction = player.status
 
+        #graphic
         self.image = pygame.Surface((40, 40))
-        self.rect = self.image.get_rect(center = player.rect.center)
+
+        #placment
+        if direction == 'right':
+            self.rect = self.image.get_rect(midleft = player.rect.midright + pygame.math.Vector2(0, 16))
+        elif direction == 'left':
+            self.rect = self.image.get_rect(midright = player.rect.midleft + pygame.math.Vector2(0, 16))
+        else:
+            self.rect = self.image.get_rect(center = player.rect.center)
+
